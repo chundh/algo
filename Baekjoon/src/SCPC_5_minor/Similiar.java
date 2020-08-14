2dd2931d4574fb6ed34664847a08e8ed0145f152
chundh <dhchun0716@gmail.com>
2020-08-14 19:34:39 +0900

    Update Similiar.java

--- Baekjoon/src/SCPC_5_minor/Similiar.java
+++ Baekjoon/src/SCPC_5_minor/Similiar.java
@@ -16,71 +16,63 @@ public class Similiar {
             n = sc.nextInt();
             arr1 = new int[n];
             arr2 = new int[n];
+            int[] dp = new int[n];
             for(int i=0; i<n; i++){
                 arr1[i] = sc.nextInt();
             }
             for(int i=0; i<n; i++){
                 arr2[i] = sc.nextInt();
+                if(i==0){
+                    if(arr1[0] == arr2[0])
+                        dp[0] = 1;
+                    else
+                        dp[0] = 0;
+                }else{
+                    if(arr1[i] == arr2[i])
+                        dp[i] = dp[i-1]+1;
+                    else
+                        dp[i] = dp[i-1];
+                }
             }
-            for(int i=0; i<n; i++){
-                solution(i,i+1);
+            for(int i=0; i<n-1; i++){
+                for(int j=i+1; j<n; j++){
+                    int ans = DPReverse(i, j);
+                    if(i!=0){
+                        ans += dp[i-1];
+                    }
+                    ans+=dp[n-1]-dp[j];
+                    if(answer<ans) {
+                        answer = ans;
+                    }
+                }
             }
             System.out.println("Case #"+(test_case+1));
             System.out.println(answer);
         }
     }
 
-    public static void solution(int start, int index){
-        for(int i=index; i<n; i++){
-            swap(start, i);
+
+    public static int DPReverse(int start, int end){
+        if(start==end){
+            if(arr1[start] == arr2[end])
+                return 1;
+            return 0;
         }
-    }
-    public static void swap(int start, int end){
-        int f_end = end;
-        int[] temp_arr = new int[n];
-        int cnt=0;
-        for(int i=0; i<start; i++){
-            temp_arr[i] = arr2[i];
-            if(arr1[i]==temp_arr[i])
-                cnt++;
-        }
-        if((start+end)%2==0){
-            int mid = (start+end)/2;
-            for(int i=start; i<mid; i++){
-                temp_arr[i] = arr2[end];
-                temp_arr[end] = arr2[i];
-                if(arr1[end] == temp_arr[end])
-                    cnt++;
-                end--;
-                if(arr1[i]==temp_arr[i])
-                    cnt++;
+        else if(start==end-1){
+            if(arr1[start] == arr2[end]){
+                return 2;
             }
-            temp_arr[mid] = arr2[mid];
-            if(arr1[mid] == temp_arr[mid])
-                cnt++;
+            return 0;
         }
-        else{
-            int mid = (start+end)/2;
-            for(int i=start; i<=mid; i++){
-                temp_arr[i] = arr2[end];
-                temp_arr[end] = arr2[i];
-                if(arr1[end] == temp_arr[end])
-                    cnt++;
-                end--;
-                if(arr1[i]==temp_arr[i])
-                    cnt++;
-            }
+        else if(start>end){
+            return 0;
         }
-        for(int i=f_end+1; i<n; i++){
-            temp_arr[i] = arr2[i];
-            if(temp_arr[i] == arr1[i])
-                cnt++;
-        }
-        if(answer<cnt) {
-            answer = cnt;
-        }else{
-            if(f_end==n-1)
-                n--;
-        }
+        int answer = 0;
+        if(arr1[start] == arr2[end])
+            answer++;
+        if(arr1[end] == arr2[start])
+            answer++;
+        answer += DPReverse(start+1, end-1);
+        return answer;
     }
 }
