94c356cbb82e1f47009a23ae88415d6279164257
chundh <dhchun0716@gmail.com>
2020-09-03 21:33:21 +0900

    Update 멀쩡한사각형.java

--- Baekjoon/src/Summer_Winter_2019/멀쩡한사각형.java
+++ Baekjoon/src/Summer_Winter_2019/멀쩡한사각형.java
@@ -4,49 +4,29 @@ public class 멀쩡한사각형 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int w = 8;
-		int h = 12;
+		long w = 8;
+		long h = 12;
 		
 		solution(w,h);
 	}
 
-	public static void solution(double w, double h) {
-		int[][] data = new int[(int) w][(int) h];
-		double flag = h/w;
-		int[][] check = new int[(int) w+1][(int) h+1];
+	public static void solution(long w, long h) {
 		long answer = 0;
-		for(int i=0; i<w+1; i++) {
-			if(flag*i == 0) {
-				answer++;
-				check[i][i] = -1;
-			}
-			else if(Integer.valueOf(String.valueOf(flag*i).split("\\.")[1]) != 0) {  //분수이면 
-				if(check[i][i-1] != -1) {
-					answer++;
-					check[i][i-1] = -1;
-					System.out.println(i + " , " + (i-1));
-				}
-				if(check[i][i] != -1) {
-					answer++;
-					check[i][i] = -1;
-					System.out.println(i + " , " + (i));
-				}
-			}
-			else {
-				if(check[i][i-1] != -1) {
-					answer++;
-					check[i][i-1] = -1;
-					System.out.println(i + " , " + (i-1));
-				}
-				if(i!=w) {
-					if(check[i+1][i] != -1) {
-						answer++;
-						check[i+1][i] = -1;
-						System.out.println((i+1) + " , " + i);
-					}
-				}
-			}
+		long min = (long)h;
+		long max = (long)w;
+		if (w < h) {
+			min = (long)w;
+			max = (long)h;
 		}
-		System.out.println(answer);
+
+		long value = 1;
+		while (value > 0) {
+			value = max % min;
+			max = min;
+			min = value;
+		}
+
+		answer = (long)w * (long)h - ((long)w + (long)h - max);
+//		return answer;
 	}
 }
