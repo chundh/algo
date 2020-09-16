19838873a0b48f092c380207fffbc877faacad00
chundh <dhchun0716@gmail.com>
2020-09-16 16:44:21 +0900

    Update 백준_6198_옥상정원.java

--- Baekjoon/src/stack/queue/백준_6198_옥상정원.java
+++ Baekjoon/src/stack/queue/백준_6198_옥상정원.java
@@ -3,6 +3,11 @@ package stack.queue;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * Monotone Stack
+ * stack을 오름차순, 내림차순으로 정렬상태를 유지하여 임의의 위치 x보다 작거나 큰 왼쪽에 있는 첫 수의 위치를 알 수 있다.
+ */
+
 public class 백준_6198_옥상정원 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
