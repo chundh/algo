11aee4f4d2c058615f4a45da26dcf34f8ff36870
chundh <dhchun0716@gmail.com>
2020-09-18 01:36:05 +0900

    Update baek_1005.java

--- Baekjoon/src/Dynamic_Programming/baek_1005.java
+++ Baekjoon/src/Dynamic_Programming/baek_1005.java
@@ -1,73 +1,73 @@
 package Dynamic_Programming;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class baek_1005 {
-    public static void main(String[] args) throws IOException {
+    static int[] arr;
+    static int ans;
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-//        int t = sc.nextInt();
-        int t = Integer.parseInt(br.readLine());
+        int t = sc.nextInt();
         for(int tc=0; tc<t; tc++){
-//            int n = sc.nextInt();
-//            int k = sc.nextInt();
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            int[] arr= new int[n+1];
-            st = new StringTokenizer(br.readLine());
-            for(int i=1; i<=n; i++){
-
-//                arr[i] = sc.nextInt();
-                arr[i] = Integer.parseInt(st.nextToken());
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            List<List<Integer>> array = new ArrayList<List<Integer>>();
+            int[] indegree = new int[n+1];
+            for(int i=0; i<=n; i++){
+                array.add(new ArrayList<>());
+                indegree[i] = 0;
             }
+            arr= new int[n+1];
             data[] data = new data[n+1];
-            for(int i=1; i<n+1; i++){
+            for(int i=1; i<=n; i++){
+                arr[i] = sc.nextInt();
                 data[i] = new data();
             }
             for(int i=0; i<k; i++){
-                st = new StringTokenizer(br.readLine());
-//                int ver = sc.nextInt();
-//                int node = sc.nextInt();
-                int ver = Integer.parseInt(st.nextToken());
-                int node = Integer.parseInt(st.nextToken());
-                data[node].arr.add(ver);
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                array.get(a).add(b);
+                indegree[b]++;
             }
-//            int obj = sc.nextInt();
-            int obj = Integer.parseInt(br.readLine());
-            int[] check = new int[n+1];
-            System.out.println(solution(arr, check, data, obj));
+            ans = sc.nextInt();
+            int[] dp = new int[n+1];
+            topologicalSort(indegree, array, dp);
         }
+    }
+    static void topologicalSort(int[] indegree, List<List<Integer>> array, int[] time) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        Queue<Integer> result = new LinkedList<Integer>();
+
+        // 큐에 indegree 가 0 인 노드 담기
+        for (int i = 1; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+                time[i] = arr[i];
+            }
+        }
+        /**
+         * 1. 큐에서 값을 꺼내며 해당 노드가 가리키는 노드의 indegree 를 1 감소
+         * 2. 만약 indegree가 0 이 된다면 큐에 넣기
+         * 3. 큐가 빌때까지 반복
+         */
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            result.offer(node);
+            for (Integer i : array.get(node)) {
+                indegree[i]--;
+                time[i] = Math.max(time[i], time[node]+arr[i]);
+                if (indegree[i] == 0) {
+                    q.offer(i);
+                }
+            }
+        }
+        System.out.println(time[ans]);
     }
 
-    public static int solution(int[] arr, int[] check, data[] data, int node){
-        if(check[node]!=0)
-            return check[node];
-        if(data[node].arr.size()==0){
-            check[node] = arr[node];
-            return arr[node];
-        }
-        int time = 0;
-        for(int i=0; i<data[node].arr.size(); i++){
-            int temp = solution(arr, check, data, data[node].arr.get(i));
-            if(temp>time)
-                time = temp;
-        }
-        time+=arr[node];
-        check[node] = time;
-        return time;
-    }
 }
 
 class data{
-    ArrayList<Integer> arr;
-    public data(){
-        this.arr = new ArrayList<>();
-    }
+    ArrayList<Integer> next = new ArrayList<>();
+    int start_time = 0;
 }
