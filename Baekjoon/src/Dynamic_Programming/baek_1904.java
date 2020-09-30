ada0664da806b3b04d396e84490027b22ad25267
chundh <dhchun0716@gmail.com>
2020-09-30 14:54:22 +0900

    Update baek_1904.java

--- Baekjoon/src/Dynamic_Programming/baek_1904.java
+++ Baekjoon/src/Dynamic_Programming/baek_1904.java
@@ -14,15 +14,16 @@ public class baek_1904 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[] arr = new long[n+1];
-        arr[0] = 0;
-        arr[1] = 1;
-        arr[2] = 2;
-        if(n>=3) {
+        long[] dp = new long[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        if (n >= 2) {
+            dp[2] = 2;
             for (int i = 3; i <= n; i++) {
-                arr[i] = (arr[i-1] + arr[i-2])%15746;
+                dp[i] = dp[i - 1] + dp[i - 2];
+                dp[i] %= 15746;
             }
         }
-        System.out.println(arr[n]);
+        System.out.println(dp[n]);
     }
 }
