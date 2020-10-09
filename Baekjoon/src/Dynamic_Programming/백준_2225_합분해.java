b16139d968819403d3e995fa3ac0b579632641d2
chundh <dhchun0716@gmail.com>
2020-10-09 18:51:04 +0900

    Create 백준_2225_합분해.java

---
+++ Baekjoon/src/Dynamic_Programming/백준_2225_합분해.java
@@ -0,0 +1,16 @@
+package Dynamic_Programming;
+
+import java.util.Scanner;
+
+public class 백준_2225_합분해 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] dp = new int[k+1];
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+
+        }
+    }
+}
