87382aaec8f1027a2fff9b9b580e9c06e3ee9907
chundh <dhchun0716@gmail.com>
2020-10-11 13:38:55 +0900

    Update 탑.java

--- Baekjoon/src/stack/queue/탑.java
+++ Baekjoon/src/stack/queue/탑.java
@@ -1,28 +1,28 @@
-package stack.queue;
-
-public class ž {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] heights = {1,5,3,6,7,6,5};
-		int[] answer = new int[heights.length];
-		int flag=0;
-		for(int i=heights.length-1; i>=0; i--) {
-			for(int j=i-1; j>=0; j--) {
-				if(heights[i]<heights[j]) {
-					answer[i] = j+1;
-					flag=1;
-					break;
-				}
-			}
-			if(flag==0) {
-				answer[i] = 0;
-			}
-		}
-		
-		for(int i=0; i<heights.length; i++) {
-			System.out.println(answer[i]);
-		}
-	}
-
-}
+//package stack.queue;
+//
+//public class 탑 {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		int[] heights = {1,5,3,6,7,6,5};
+//		int[] answer = new int[heights.length];
+//		int flag=0;
+//		for(int i=heights.length-1; i>=0; i--) {
+//			for(int j=i-1; j>=0; j--) {
+//				if(heights[i]<heights[j]) {
+//					answer[i] = j+1;
+//					flag=1;
+//					break;
+//				}
+//			}
+//			if(flag==0) {
+//				answer[i] = 0;
+//			}
+//		}
+//
+//		for(int i=0; i<heights.length; i++) {
+//			System.out.println(answer[i]);
+//		}
+//	}
+//
+//}
