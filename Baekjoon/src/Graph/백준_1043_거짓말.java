d8ff4d68f1ada5ec5971146eeecbdfad803d1714
chundh <dhchun0716@gmail.com>
2020-10-12 11:01:13 +0900

    Update 백준_1043_거짓말.java

--- Baekjoon/src/Graph/백준_1043_거짓말.java
+++ Baekjoon/src/Graph/백준_1043_거짓말.java
@@ -1,78 +1,75 @@
 package Graph;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 백준_1043_거짓말 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
+        int[] human = new int[51];
+        int[] answer = new int[m];
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        for(int i=0; i<m; i++){
+            arr.add(new ArrayList<>());
+        }
         if(k==0){
             System.out.println(m);
+            return;
         }else {
-            ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-            boolean[] check = new boolean[n+1];
-            while(st.hasMoreTokens()){
-                int temp = Integer.parseInt(st.nextToken());
-                check[temp] = true;
+            for(int i=0; i<k; i++){
+                human[Integer.parseInt(st.nextToken())] = 1;
             }
-            for(int i=0; i<=n; i++){
-                arr.add(new ArrayList<>());
-            }
-            int[][] question = new int[m][];
-            for(int tc=0; tc<m; tc++){
-                st = new StringTokenizer(br.readLine());
-                int cnt = Integer.parseInt(st.nextToken());
-                question[tc] = new int[cnt];
-                int[] check_arr = new int[cnt];
-                for(int i=0; i<cnt; i++){
-                    check_arr[i] = Integer.parseInt(st.nextToken());
-                    question[tc][i] = check_arr[i];
-                }
-                for(int i=0; i<cnt; i++){
-                    for(int j=i+1; j<cnt; j++){
-                        arr.get(check_arr[i]).add(check_arr[j]);
-                        arr.get(check_arr[j]).add(check_arr[i]);
-                    }
-                }
-            }
-            Queue<Integer> queue = new LinkedList<>();
-            for(int i=1; i<n; i++){
-                if(check[i]){
-                    queue.add(i);
-                }
-            }
-            while(!queue.isEmpty()){
-                int temp = queue.poll();
-                for(int next : arr.get(temp)){
-                    if(!check[next]){
-                        queue.add(next);
-                        check[next] = true;
-                    }
-                }
-            }
-            int answer = 0;
-            for(int i=0; i<question.length; i++){
+            for(int i=0; i<m; i++){
                 int flag=0;
-                for(int j=0; j<question[i].length; j++){
-                    if(check[question[i][j]]){
+                st = new StringTokenizer(br.readLine());
+                int size = Integer.parseInt(st.nextToken());
+                for(int j=0; j<size; j++){
+                    int num = Integer.parseInt(st.nextToken());
+                    if(human[num] == 1){ // 진실을 아는사람의 경우
                         flag=1;
-                        break;
+                        answer[i] = 1;
+                    }
+                    arr.get(i).add(num);
+                }
+                if(flag==1){
+                    for(int j=0; j<arr.get(i).size(); j++){
+                        human[arr.get(i).get(j)]  = 1;
+                    }
+                    for(int j=1; j<=50; j++){
+                        if(human[j] == 1){
+                            for(int idx=0; idx<i; idx++){
+                                if(arr.get(idx).contains(j)){
+                                    for(int l=0; l<arr.get(idx).size();l++){
+                                        human[arr.get(idx).get(l)] = 1;
+                                        answer[idx] = 1;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
-                if(flag==0){
-                    answer++;
-                }
             }
-            System.out.println(answer);
         }
+        int ans = 0;
+        for(int i=0; i<m; i++){
+            if(answer[i] == 0)
+                ans++;
+        }
+        System.out.println(ans);
     }
 }
+/*
+4 3
+1 1
+2 3 2
+1 3
+3 1 3 4
+ */
