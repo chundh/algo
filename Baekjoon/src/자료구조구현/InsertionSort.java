92a9c95a86e5dc130ba7e9eef9566d7b20e47999
chundh <dhchun0716@gmail.com>
2020-10-21 12:30:01 +0900

    Create InsertionSort.java

---
+++ Baekjoon/src/자료구조구현/InsertionSort.java
@@ -0,0 +1,25 @@
+package 자료구조구현;
+
+import java.util.Arrays;
+
+/**
+ * 선택된 index의 이전의 index를 탐색 하여 올바른 위치를 정하고 그 위치로 데이터를 삽입하며 뒤의 데이터는 뒤로 미룬다.
+ */
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {1,5,2,3,4,8,2,13,124,6,12,37};
+        int j;
+        for(int i=1; i<arr.length; i++){
+            int temp = arr[i];
+            for(j=i; j>0; j--){
+                if(arr[j-1] > temp){
+                    arr[j] = arr[j-1];
+                }else
+                    break;
+            }
+            arr[j] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
