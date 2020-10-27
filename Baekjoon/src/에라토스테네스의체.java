ba7d43f963d8700e2bd7a5d6eae9dde4e53d3a8c
chundh <dhchun0716@gmail.com>
2020-10-27 14:22:46 +0900

    Update 에라토스테네스의체.java

--- Baekjoon/src/에라토스테네스의체.java
+++ Baekjoon/src/에라토스테네스의체.java
@@ -1,18 +1,26 @@
+import java.util.ArrayList;
+
 public class 에라토스테네스의체 {
     public static void main(String[] args) {
-        int n = 100;
-        boolean[] arr = new boolean[n]; // 소수이면 false로 저장
+        int n = 2;
+        boolean[] arr = new boolean[n+1]; // 소수이면 false로 저장
         arr[0] = true;
         arr[1] = true;
-        for(int i=2; i*i<arr.length; i++){
-            if(arr[i])
+        ArrayList<Integer> info = new ArrayList<>();
+        for(int i=2; i*i<=arr.length; i++){
+            if(arr[i]) {
                 continue;
+            }
+            info.add(i);
+            System.out.println(i);
             for(int j = i*i; j<n; j+=i){
                 arr[j] = true;
             }
         }
-        for(int i=0; i<n; i++){
-            System.out.println(i + " : " + arr[i]);
+        for(int i=2; i<=arr.length; i++){
+            if(!arr[i]){
+
+            }
         }
     }
 }
