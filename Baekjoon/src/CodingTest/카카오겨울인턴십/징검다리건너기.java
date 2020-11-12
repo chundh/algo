2a6145acc9be94afea6cd9bc4cc52d8a26301e10
chundh <dhchun0716@gmail.com>
2020-11-12 21:42:23 +0900

    Update 징검다리건너기.java

--- Baekjoon/src/CodingTest/카카오겨울인턴십/징검다리건너기.java
+++ Baekjoon/src/CodingTest/카카오겨울인턴십/징검다리건너기.java
@@ -4,12 +4,33 @@ public class 징검다리건너기 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+		int[] stones = {2, 4, 5, 3, 2, 1, 4, 6, 5, 1};
 		int k = 3;
 		
 		solution(stones, k);
 	}
-	
+	public static void solution(int[] stones, int k){
+		int answer = Integer.MAX_VALUE;
+		int idx = -1;
+		while(true){
+			if(idx+k>=stones.length)
+				break;
+			int max = Integer.MIN_VALUE;
+			int sum = 0;
+			for (int i = 1; i <= k; i++) {
+				if(max<stones[idx+i]){
+					sum = i;
+					max = stones[idx+i];
+				}
+			}
+			idx += sum;
+			System.out.println(idx + " : " + max);
+			answer = Math.min(answer, max);
+		}
+		System.out.println(answer);
+	}
+
+	/*
 	public static void solution(int[] stones, int k) {
 		int flag=0;
 		int answer = 0;
@@ -34,5 +55,5 @@ public class 징검다리건너기 {
 		}
 		System.out.println(answer);
 	}
-
+	*/
 }
